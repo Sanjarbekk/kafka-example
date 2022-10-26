0bcccf5d5117caf74a14461aac7e135195b451b5
Sanjarbekk <esonmurodov.com@gmail.com>
2022-10-26 09:37:08 +0500

    Initial Commit

---
+++ src/main/java/com/example/kafkaexample/MessageRequest.java
@@ -0,0 +1,20 @@
+package com.example.kafkaexample;
+
+public class MessageRequest {
+    String message;
+
+    public MessageRequest() {
+    }
+
+    public MessageRequest(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
